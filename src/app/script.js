9ec97e434e3545509c04effd2ea67efdf7584ba5
Aghvan <java.agvan@gmail.com>
2019-11-26 21:41:38 +0400

    initial commit

--- src/app/script.js
+++ src/app/script.js
@@ -1,9 +1,9 @@
-window.onload = function(){
-  var $ = function(e){
+window.onload = function () {
+  var $ = function (e) {
     return document.querySelector(e);
   }
 
-  var setSubColor = function(c){
+  var setSubColor = function (c) {
     $('#human-head').style.fill = c;
     $('#human-body').style.stroke = c;
   }
@@ -14,9 +14,8 @@ window.onload = function(){
   }
 
 
-
   // Function To Set Color
-   var setColor = function(e, c){
+  var setColor = function (e, c) {
     e.style.backgroundColor = c;
   }
 
@@ -31,7 +30,15 @@ window.onload = function(){
   var yLamp = $('#y');
   var glamp = $('#g');
   var glampR = $('#green-raight');
+  var glampl = $('#green-left');
+  var rnext = $('#greenn-raight');
   // Lsmp Colors
+
+  //three trafic
+  var bridgeYellow = $('#yelloww');
+  var bridgeRed = $('#redd');
+  var bridgeGren =$('#greenn');
+
 
   var green = "#00EE00";
   var yellow = "#DDDD00";
@@ -44,9 +51,9 @@ window.onload = function(){
   var yOn = new isOn();
   var rOn = new isOn();
 
-  function check(n){
+  function check(n) {
     n = parseInt(n);
-    while(isNaN(n)){
+    while (isNaN(n)) {
       n = parseInt(prompt("enter a valid number!"));
     }
     return Math.floor(n);
@@ -56,20 +63,20 @@ window.onload = function(){
 
   var redTimer = check(22); // Time For Red Light
   var yelTimer = check(3);// Time For Yellow Light
-  var greTimer = check(22);// Time For Green Light
+  var greTimer = check(35);// Time For Green Light
 
   var totalTime = greTimer + yelTimer + redTimer;
   var timeCount = greTimer;
 
 
-  if(greTimer && yelTimer && redTimer){
+  if (greTimer && yelTimer && redTimer) {
     var i = 1;
-    var intr = setInterval(function(){
-      if(i <= greTimer){
-        if(rOn.getOn()){
+    var intr = setInterval(function () {
+      if (i <= greTimer) {
+        if (rOn.getOn()) {
           timeCount = greTimer;
 
-          }
+        }
 
         yOn.setOn(false);
         rOn.setOn(false);
@@ -86,16 +93,46 @@ window.onload = function(){
         setColor(glamp, gDim);
         setColor(yLamp, yDim);
         setColor(glampR, gDim);
+        setColor(glampl, green);
+        setColor(rnext, green);
+        setColor(bridgeRed,rDim);
+        setColor(bridgeYellow,yDim);
+        setColor(bridgeGren, gDim);
+        setColor(rnext, green);
 
-        if(timeCount <= 14){
-          setColor(glampR, green);
+        if (greTimer) {
+          setColor(bridgeGren, gDim);
+          setColor(rnext, green);
+          setColor(bridgeRed ,red);
+          if (timeCount <= 23) {
+            if (timeCount == 23 || timeCount ==21 || timeCount <= 19) {
+              setColor(rnext, gDim);
+              setColor(glampl, gDim);
+
+              if(timeCount <= 19){
+                setColor(rLamp,rDim);
+                setColor(yLamp,yDim);
+                setColor(glamp, green);
+              }
+            }
+            if(19 < timeCount <= 22){
+              setColor(rLamp,rDim);
+              setColor(yLamp,yellow);
+            }
+            if(timeCount <= 19){
+              setColor(yLamp, yDim);
+            }
+            setColor(glampR, green);
+
+            setColor(rLamp,rDim);
+          }
         }
 
         setSubColor("red");
-        setSubColorR("green");
+        setSubColorR("red");
 
-      }else if(i > greTimer && i <= greTimer + yelTimer){
-        if(gOn.getOn()){
+      } else if (i > greTimer && i <= greTimer + yelTimer) {
+        if (gOn.getOn()) {
           timeCount = yelTimer;
         }
 
@@ -110,15 +147,16 @@ window.onload = function(){
         setColor(greLamp, gDim);
         setColor(redLamp, rDim);
         setColor(yelLamp, yellow);
-
-        setColor(rLamp,rDim);
+        setColor(bridgeYellow, yellow);
+        setColor(rLamp, rDim);
         setColor(glamp, gDim);
         setColor(yLamp, yellow);
-
+        setColor(bridgeRed,rDim);
+        setColor(glampR,gDim);
         setSubColor("red");
         setSubColorR("red");
-      }else{
-        if(yOn.getOn()){
+      } else {
+        if (yOn.getOn()) {
           timeCount = redTimer;
         }
 
@@ -128,28 +166,35 @@ window.onload = function(){
 
         timerNum.innerHTML = timeCount--;
 
-        if(timeCount == redTimer-1) {
+        if (timeCount == redTimer - 1) {
           setSubColor("green");
-          setSubColorR("red");
+          setSubColorR("green");
           setColor(yelLamp, yDim);
           setColor(greLamp, gDim);
           setColor(redLamp, red);
-          if(redTimer){
-            console.log(redTimer);
-            setColor(glampR, gDim);
+          setColor(bridgeYellow, yDim);
+          if (redTimer) {
+            setColor(bridgeGren, green);
+            setColor(glampR, green);
+            setColor(rnext, green);
+
           }
-          setColor(rLamp, rDim);
-          setColor(glamp, green);
+          setColor(rLamp, red);
+          setColor(glamp, gDim);
           setColor(yLamp, yDim);
+          setColor(bridgeYellow, yDim);
         }
-        if(timeCount == 1){
+        if (timeCount == 1) {
           setSubColor("red");
           setSubColorR("red");
           setColor(yelLamp, yellow);
+          setColor(bridgeYellow, yellow);
           setColor(greLamp, gDim);
           setColor(redLamp, rDim);
-
-          setColor(rLamp,rDim);
+          setColor(bridgeGren, gDim);
+          setColor(rnext,gDim);
+          setColor(glampR, gDim);
+          setColor(rLamp, rDim);
           setColor(glamp, gDim);
           setColor(yLamp, yellow);
         }
@@ -158,10 +203,10 @@ window.onload = function(){
       i++;
       // If Done, Repeat
 
-      if(i-1 >= totalTime)
+      if (i - 1 >= totalTime)
         i = 0;
     }, 1000);
-  }else{
+  } else {
     alert("error, don't input 0")
   }
   // Update Every 1 seconds
@@ -169,12 +214,12 @@ window.onload = function(){
 }
 
 // Helper
-var isOn = function(){
+var isOn = function () {
   this.on = false;
-  this.setOn = function(b){
+  this.setOn = function (b) {
     this.on = b;
   }
-  this.getOn = function(){
+  this.getOn = function () {
     return this.on;
   }
 }
